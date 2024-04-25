c6820810f3463ecc2b606d97298c3532ea74b40a
Flacko <lesliejoe2k2@gmail.com>
2024-04-25 19:37:04 +0100

    new feat: todo date

--- todo-list/script.js
+++ todo-list/script.js
@@ -2,21 +2,27 @@ const inputItem = document.querySelector('input');
 const addBtn = document.querySelector('.add-btn');
 const itemsDiv = document.querySelector('.todo-list-items')
 
-
+const date = document.getElementById('date-select');
 
 const todoList = []
+
+const todo = document.querySelector('.todo')
+const dateTodo = document.querySelector('.date')
 
 const getInputData = () =>{
     const itemAdded = inputItem.value
     todoList.push(itemAdded)
     inputItem.value = ''
+    todo.textContent = todoList[0]
+    dateTodo.textContent = date.value
+    
 
-    itemsDiv.innerHTML = ''; 
-    todoList.forEach(element => {
-        const p = document.createElement('p');
-        p.textContent = element;
-        itemsDiv.appendChild(p)
-    });
+    // itemsDiv.innerHTML = ''; 
+    // todoList.forEach(element => {
+    //     const p = document.createElement('p');
+    //     p.textContent = element;
+    //     itemsDiv.appendChild(p)
+    // });
 }
 
 addBtn.addEventListener('click', getInputData)
