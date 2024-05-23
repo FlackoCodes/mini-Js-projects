8fb36c1530fac95c4431953a4617141f13c9650d
Flacko <lesliejoe2k2@gmail.com>
2024-05-23 21:44:54 +0000

    new feat: get date from order

--- amazon/data/cart.js
+++ amazon/data/cart.js
@@ -1,22 +1,36 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) 
-
-if (!cart) {
-    cart = [
-        {
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-        },
-        {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 2,
-        }
-    ]
-}
+// export let cart = JSON.parse(localStorage.getItem('cart')) 
 
 
-const saveToLocalStorage = ()=>{
-    localStorage.setItem('cart', JSON.stringify(cart))
-}
+// if (!cart) {
+//     cart = [
+//         {
+//             productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+//             quantity: 2,
+//         },
+//         {
+//             productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+//             quantity: 2,
+//         }
+//     ]
+// }
+
+
+// const saveToLocalStorage = ()=>{
+//     localStorage.setItem('cart', JSON.stringify(cart))
+// }
+ 
+export let cart = [
+            {
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 2,
+                deliveryOptionsId: '1'
+            },
+            {
+                productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 2,
+                deliveryOptionsId: '2'
+            }
+        ]
 
 export const addToCart = (productId, productName)=>{
     let matchingCartItem;
@@ -35,9 +49,9 @@ export const addToCart = (productId, productName)=>{
         })
     } else{
         matchingCartItem.quantity ++;
-    }
+    } 
 
-    saveToLocalStorage()
+    // saveToLocalStorage()
     
 }
 
@@ -53,7 +67,7 @@ export const removeItemFromCart = (productId)=>{
             newCart.push(cartItem)
         } 
         cart = newCart;
-        saveToLocalStorage()
+        // saveToLocalStorage()
     })
 
 }
